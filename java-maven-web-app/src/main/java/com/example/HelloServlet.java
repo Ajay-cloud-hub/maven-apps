4b8163e0795c80b9b5e9fe88579e4f5eb47bae44
Ajay <ajaykumarbasineni2025@example.com>
2026-06-03 13:37:13 +0530

    first commit

---
+++ java-maven-web-app/src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,13 @@
+package com.example;
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+public class HelloServlet extends HttpServlet {
+ protected void doGet(HttpServletRequest req,HttpServletResponse res)
+ throws ServletException, IOException {
+   res.setContentType("text/html");
+   PrintWriter out = res.getWriter();
+   out.println("<h1>Hello Java Maven Web App!</h1>");
+ }
+}
